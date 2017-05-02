64c95c7014be534e5a7c5756e58092906074d9ed
polinchakb <polinchakb@elnk.com>
2017-05-02 15:42:59 -0400

    Added more automations in the scripts.

--- docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
+++ docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
@@ -16,13 +16,20 @@ echo '$AWS_VPC_ID = ' $3
 echo '$APP_NAME = ' $4
 echo '$NODES = ' $5
 
-echo "Creating Swarm Master"
+echo "Creating Swarm Master...."
 docker-machine create --driver amazonec2 --amazonec2-vpc-id $AWS_VPC_ID $APP_NAME-swarm-master
 
-echo "Creating Swarm Instances"
+
+echo "Creating Swarm Instances..."
 COUNTER=0
 while [  $COUNTER -lt $NODES ]; do
        docker-machine create --driver amazonec2 --amazonec2-vpc-id $AWS_VPC_ID $APP_NAME-node-$COUNTER
        echo The counter is $COUNTER
        let COUNTER=COUNTER+1
 done
+
+echo "swarm master IP:"
+docker-machine ssh $APP_NAME-swarm-master 'ifconfig | grep "inet addr" | grep "255.255.255.0"'
+echo "Run these commands:"
+echo "docker-machine ssh $APP_NAME-swarm-master 'sudo docker swarm init --advertise-addr IP-ADDRESS-OF-SWARM-MASTER'"
+echo "docker-machine ssh $APP_NAME-node-0 'sudo docker swarm join --token TOKEN_FROM_THE_MASTER_SECTION IP-ADDRESS-OF-SWARM-MASTER:2377'"
