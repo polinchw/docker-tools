e14025aa1ae6273dae1ff1db7e03bdb4db8806c7
polinchw <polinchw@netscape.net>
2017-07-01 16:34:34 -0400

    Adding subnet and Github reg.

--- docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
+++ docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
@@ -4,9 +4,11 @@ AWS_SECRET_ACCESS_KEY=$2
 AWS_VPC_ID=$3
 APP_NAME=$4
 NODES=$5
+APP_REG=$6
+SUB_NET=$7
 
-if [ -z "$AWS_ACCESS_KEY_ID"  ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_VPC_ID" ] || [ -z "$APP_NAME" ]; then
-  echo "usage: ./create-swarm-instances.sh <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_VPC_ID> <APP_NAME> <NODES>"
+if [ -z "$AWS_ACCESS_KEY_ID"  ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_VPC_ID" ] || [ -z "$APP_NAME" ] || [ -z "$APP_REG" ] || [ -z "$SUB_NET" ]; then
+  echo "usage: ./create-swarm-instances.sh <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_VPC_ID> <APP_NAME> <NODES> <APP_REG> <SUB_NET>"
   exit 1;
 fi
 
@@ -15,9 +17,11 @@ echo '$AWS_SECRET_ACCESS_KEY = ' $2
 echo '$AWS_VPC_ID = ' $3
 echo '$APP_NAME = ' $4
 echo '$NODES = ' $5
+echo '$APP_REG = ' $6
+echo '$SUB_NET = ' $7
 
 echo "Creating Swarm Master...."
-docker-machine create --driver amazonec2 --amazonec2-vpc-id $AWS_VPC_ID $APP_NAME-swarm-master
+docker-machine create --driver amazonec2 --amazonec2-vpc-id $AWS_VPC_ID --amazonec2-subnet-id $SUB_NET $APP_NAME-swarm-master
 
 
 echo "Creating Swarm Instances..."
@@ -45,4 +49,4 @@ echo "$INIT_COMMAND_RESULTS"
 echo "Run this command to add instances to the swarm:"
 echo "docker-machine ssh $APP_NAME-node-0 'sudo docker swarm join --token TOKEN_FROM_THE_MASTER_SECTION $MASTER_INTERNAL_IP:2377'"
 echo "Run this command to add a Docker Service to the swarm:"
-echo "docker-machine ssh $APP_NAME-swarm-master 'sudo docker service create --replicas 2 --name $APP_NAME -p:8080:8080 polinchw/$APP_NAME'"
+echo "docker-machine ssh $APP_NAME-swarm-master 'sudo docker service create --replicas 2 --name $APP_NAME -p:8080:8080 $APP_REG/$APP_NAME'"
