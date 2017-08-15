cbcc627d7b22bc187dc3c3eef137ad9e231ed299
polinchakb <polinchakb@elnk.com>
2017-08-15 12:52:40 -0400

    Printing out the bits.

--- docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
+++ docker-machine/docker-swarm/aws/bash-scripts/create-swarm-instances.sh
@@ -49,7 +49,7 @@ echo "swarm init results:"
 #    --token SWMTKN-1-01ifqiy6q1nrq8uh48ajen568058sxz9oo78bge7taxqrfgqqa-ba7swkzlr0segdflxx1r2d81m \
 #    10.0.1.67:2377
 echo "$INIT_COMMAND_RESULTS"
-IFS=' ' read -ra BITS <<< "$INIT_COMMAND_RESULTS"    #Convert string to array
+IFS='\s' read -ra BITS <<< "$INIT_COMMAND_RESULTS"    #Convert string to array
 #Print all names from array
 for i in "${BITS[@]}"; do
     echo "Bit: " $i
